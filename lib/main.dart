0e4447ef9dc1c8c66e96809f2de8c165c790e6e1
Kwakzz <kwakuosafo20@gmail.com>
2023-12-12 15:40:11 +0000

    updated pubspec

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,7 @@ import 'package:ashesi_bus/pages/account/login.dart';
 import 'package:ashesi_bus/pages/nav_tabs/entry.dart';
 import 'package:ashesi_bus/models/bus_user_pref.dart';
 import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
+
 
 // check if user is logged in
 // if logged in, show entry page
@@ -15,9 +14,6 @@ import 'firebase_options.dart';
 void main() async {
 
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
 
   runApp(
     MaterialApp(
